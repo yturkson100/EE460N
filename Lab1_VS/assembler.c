425bb352e997b685723cbe08e96a298269e4d359
Yaw Turkson <yturk@DESKTOP-G0FHGS4>
2018-01-26 15:12:52 -0600

    this is what i have so far. testing

--- Lab1_VS/assembler.c
+++ Lab1_VS/assembler.c
@@ -1,11 +1,126 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include <stdio.h> /* standard input/output library */
 #include <stdlib.h> /* Standard C Library */
 #include <string.h> /* String operations library */
 #include <ctype.h> /* Library for useful character operations */
-#include <limits.h> /* Library for definitions of common variable type characteristics */}
+#include <limits.h> /* Library for definitions of common variable type characteristics */
 
-int main() {
-	printf("hopefully, this works. GG\n");
+const char *opcode[] = { "add","and","halt","jmp","jsr","jsrr","ldb","ldw","lea","nop","not","ret","lshf", "rshfl",
+"rshfa","rti","stb","stw","trap","xor","brn","brzp","brz","brnp","brp","brnz","br","brnzp" };
+#define MAX_LINE_LENGTH 255
+enum { DONE, OK, EMPTY_LINE };
+FILE* infile = NULL;
+FILE* outfile = NULL;
 
-	printf("My turn");	//Farhan
+int parseArgs(int argc, char* argv[]) {
+	if (argc == 3) {
+		char *prgName = NULL;
+		char *iFileName = NULL;
+		char *oFileName = NULL;
+
+		prgName = argv[0];
+		iFileName = argv[1];
+		oFileName = argv[2];
+
+		printf("program name = '%s'\n", prgName);
+		printf("input file name = '%s'\n", iFileName);
+		printf("output file name = '%s'\n", oFileName);
+
+		return 1;
+
+	}
+	else {
+		printf("invalid number of inputs\n");
+		return 0;
+	}
+}
+
+int isOpcode(char* text) {
+	for (int i = 0; i<28; i++) {
+		if (!strcmp(text, opcode[i])) {
+			return 1;
+		}
+	}
+
+	return -1;
+}
+
+int readAndParse(FILE * pInfile, char * pLine, char ** pLabel, char
+	** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4
+) {
+	char * lRet, *lPtr;
+	int i;
+	if (!fgets(pLine, MAX_LINE_LENGTH, pInfile))
+		return(DONE);
+	for (i = 0; i < strlen(pLine); i++)
+		pLine[i] = tolower(pLine[i]);
+
+	/* convert entire line to lowercase */
+	*pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
+
+	/* ignore the comments */
+	lPtr = pLine;
+
+	while (*lPtr != ';' && *lPtr != '\0' &&
+		*lPtr != '\n')
+		lPtr++;
+
+	*lPtr = '\0';
+	if (!(lPtr = strtok(pLine, "\t\n ,")))
+		return(EMPTY_LINE);
+
+	if (isOpcode(lPtr) == -1 && lPtr[0] != '.') /* found a label */
+	{
+		*pLabel = lPtr;
+		if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
+	}
+
+	*pOpcode = lPtr;
+
+	if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
+
+	*pArg1 = lPtr;
+
+	if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
+
+	*pArg2 = lPtr;
+	if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
+
+	*pArg3 = lPtr;
+
+	if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
+
+	*pArg4 = lPtr;
+
+	return(OK);
+}
+
+
+int main(int argc, char* argv[]) {
+	if (parseArgs(argc, argv)) {
+		/* open the source file */
+		infile = fopen(argv[1], "r");
+		outfile = fopen(argv[2], "w");
+
+		if (!infile) {
+			printf("Error: Cannot open file %s\n", argv[1]);
+			exit(4);
+		}
+		if (!outfile) {
+			printf("Error: Cannot open file %s\n", argv[2]);
+			exit(4);
+		}
+
+		/* Do stuff with files */
+		printf("successfully did things with files\n");
+
+		fclose(infile);
+		fclose(outfile);
+
+	}
+
+
 }
